4f9af8b04319f821027352a8de325d4a7285c36c
wukuohao <wukuohao2022@icloud.com>
2024-01-22 11:55:25 +0800

    neovim

---
+++ lua/super_install.lua
@@ -0,0 +1,37 @@
+local istl = {}
+
+local function open_window(istl)
+	local buf = vim.api.nvim_create_buf(true, true)
+	local opts = {
+		relative = "editor",
+		title = "SuperInstaller",
+		col = istl.position.x,
+		row = istl.position.y,
+		style = "minimal",
+		border = "rounder",
+		title_pos = "center",
+	}
+	vim.api.nvim_open_win(buf, true, opts)
+end
+
+local function setup(option)
+	local ui = vim.api.nvim_list_uis()[1]
+	istl = vim.tbl_extend("force", {
+		display = {
+			float = true,
+			width = ui.width / 2,
+			height = ui.height / 2,
+			position = {
+				x = "center",
+				y = "center",
+			},
+		},
+		ist_path = "",
+		ist_methods = "HTTPS",
+	}, option or {})
+	vim.keymap.set("n", "<leader>st", open_window(istl))
+end
+
+return {
+	setup = setup,
+}
