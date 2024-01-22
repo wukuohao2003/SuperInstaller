7944b202e5c81a329d55fb9cd9d0e9bdc24a12db
wukuohao <wukuohao2022@icloud.com>
2024-01-22 13:41:11 +0800

    test

--- lua/super_install.lua
+++ lua/super_install.lua
@@ -1,25 +1,9 @@
 local istl = {}
 
-local function open_window(width, height, ui)
-	vim.notify("我成功执行了哦")
-	local buf = vim.api.nvim_create_buf(true, true)
-	local opts = {
-		relative = "editor",
-		title = "SuperInstaller",
-		col = (ui.width - width) / 2,
-		row = (ui.height - height) / 2,
-		style = "minimal",
-		border = "rounder",
-		title_pos = "center",
-	}
-	vim.api.nvim_open_win(buf, true, opts)
-end
-
 local function setup(option)
 	local ui = vim.api.nvim_list_uis()[1]
 	local main = vim.tbl_extend("force", {
 		display = {
-			float = true,
 			width = ui.width / 2,
 			height = ui.height / 2,
 			position = {
@@ -30,8 +14,19 @@ local function setup(option)
 		ist_path = "",
 		ist_methods = "HTTPS",
 	}, option or {})
+
 	vim.keymap.set("n", "<C-T>", function()
-		open_window(main.display.width, main.display.height, ui)
+		local buf = vim.api.nvim_create_buf(true, true)
+		local opts = {
+			relative = "editor",
+			title = "SuperInstaller",
+			col = (ui.width - main.display.width) / 2,
+			row = (ui.height - main.display.height) / 2,
+			style = "minimal",
+			border = "rounder",
+			title_pos = "center",
+		}
+		vim.api.nvim_open_win(buf, true, opts)
 	end, {})
 end
 
