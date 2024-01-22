c2f001aedc63637e9a01090640821d7d563ac25f
wukuohao <wukuohao2022@icloud.com>
2024-01-22 16:24:29 +0800

    test

---
+++ lua/SuperInstaller/init.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+M.setup = function(config)
+	local configure = vim.tbl_extend("force", {
+		install_methods = "ssh",
+		display = {
+			progress_bar = true,
+			title = "",
+			title_pos = "center",
+			size = {
+				width = 60,
+				height = 7,
+			},
+			position = {
+				pos = "cc",
+			},
+			color = {
+				bar_color = "",
+				font_color = "",
+			},
+		},
+		use = {},
+	}, config or {})
+	vim.notify("我已成功加载")
+end
+
+local function install() end
+local function updae() end
+local function clean() end
+
+return {
+	setup = M.setup,
+}
