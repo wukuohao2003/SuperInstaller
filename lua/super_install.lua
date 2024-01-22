9566f688b80f9ca531e4639622f9f8248fdf5317
wukuohao <wukuohao2022@icloud.com>
2024-01-22 15:33:42 +0800

    test

--- lua/super_install.lua
+++ lua/super_install.lua
@@ -1,30 +1,30 @@
-local main = {}
-
-local function setup(option)
-	local ui = vim.api.nvim_list_uis()[1]
-	main = vim.tbl_extend("force", {
+local function setup(config)
+	local configure = vim.tbl_extend("force", {
+		install_methods = "ssh",
 		display = {
-			width = ui.width / 2,
-			height = ui.height / 2,
-		},
-		ist_path = "",
-		ist_methods = "HTTPS",
-		istl_plugin = {},
-	}, option or {})
-	vim.keymap.set("n", "<C-T>", function()
-		local buf = vim.api.nvim_create_buf(true, true)
-		local opts = {
-			relative = "editor",
-			title = "SuperInstaller",
-			col = (ui.width - main.display.width) / 2,
-			row = (ui.height - main.display.height) / 2,
-			style = "minimal",
-			border = "rounder",
+			progress_bar = true,
+			title = "",
 			title_pos = "center",
-		}
-		vim.api.nvim_open_win(buf, true, opts)
-	end, {})
+			size = {
+				width = 60,
+				height = 7,
+			},
+			position = {
+				pos = "cc",
+			},
+			color = {
+				bar_color = "",
+				font_color = "",
+			},
+		},
+		use = {},
+	}, config or {})
+	vim.notify("我已成功加载")
 end
+
+local function install() end
+local function updae() end
+local function clean() end
 
 return {
 	setup = setup,
