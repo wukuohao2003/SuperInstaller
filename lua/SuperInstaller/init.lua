95904c5427d7b04db305fc033f19620dbc2f5c9a
wukuohao <wukuohao2022@icloud.com>
2024-01-22 23:48:20 +0800

    test

--- lua/SuperInstaller/init.lua
+++ lua/SuperInstaller/init.lua
@@ -2,15 +2,26 @@ local M = {}
 
 local install_path = vim.fn.stdpath("data") .. "/site/super_installer/start"
 
-local function install(opt)
+local function Mode(mode)
+	local any = nil
+	if mode == "ssh" then
+		any = "git@gitub.com:"
+	else
+		any = "https://github.com/"
+	end
+	return any
+end
+
+local function SuperSyncdDownload(opt)
 	for _, value in ipairs(opt.plugin) do
-		vim.api.nvim_command(
-			"!git pull git@github.com:" .. value .. " " .. install_path .. "/" .. vim.split(value, "/")[2]
-		)
+		local mode = Mode(opt.mode)
+		if vim.fn.isdirectory(install_path .. vim.split(value, "/")[2]) == 0 then
+			vim.fn.system("git clone --depth=1 " .. mode .. value .. " " .. install_path)
+		else
+			return false
+		end
 	end
 end
-local function updae() end
-local function clean() end
 
 M.setup = function(config)
 	local configure = vim.tbl_extend("force", {
@@ -20,7 +31,7 @@ M.setup = function(config)
 				enable = true,
 				title = "",
 				title_pos = "center",
-				vasize = { w = 60, h = 7 },
+				size = { w = 60, h = 7 },
 				position = "cc",
 				style = {
 					f_color = "",
@@ -31,7 +42,11 @@ M.setup = function(config)
 		use = {},
 	}, config or {})
 	vim.keymap.set("n", "<C-i>", function()
-		install({ progress_bar = configure.display.progress_bar.enable, plugin = configure.use })
+		SuperSyncdDownload({
+			progress_bar = configure.display.progress_bar.enable,
+			plugin = configure.use,
+			mode = configure.install_methods,
+		})
 	end)
 end
 
