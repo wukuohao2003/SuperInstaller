9b09282d770b957f468a69cffd1b14be05cc7cae
wukuohao <wukuohao2022@icloud.com>
2024-01-22 12:10:39 +0800

    test

--- lua/super_install.lua
+++ lua/super_install.lua
@@ -29,7 +29,7 @@ local function setup(option)
 		ist_path = "",
 		ist_methods = "HTTPS",
 	}, option or {})
-	vim.keymap.set("n", "<leader>st", function()
+	vim.keymap.set("n", "<leader>aa", function()
 		open_window(main.display.width, main.display.height, ui)
 	end, {})
 end
