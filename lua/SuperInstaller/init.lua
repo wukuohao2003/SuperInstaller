2e3d76bb61d19edd3e29614e4cf4a973b5ea6fea
wukuohao <wukuohao2022@icloud.com>
2024-01-24 19:46:04 +0800

    test

--- lua/SuperInstaller/init.lua
+++ lua/SuperInstaller/init.lua
@@ -13,17 +13,63 @@ local function Mode(mode)
 	return any
 end
 
+local function installMethods(opt, use)
+	local mode = Mode(opt.mode)
+	local exists = vim.fn.isdirectory(install_path .. vim.split(use, "/")[2]) == 1
+	if exists then
+		return ("!cd " .. install_path .. "/" .. vim.split(use, "/")[2] .. " && git pull")
+	else
+		return ("!git clone " .. mode .. use .. " " .. install_path .. "/" .. vim.split(use, "/")[2])
+	end
+end
+
+local function progressInstall(opt)
+	local win_width = 40
+	local win_height = 3
+	local win_row = math.floor((vim.o.lines - win_height) / 2)
+	local win_col = math.floor((vim.o.columns - win_width) / 2)
+	local buf = vim.api.nvim_create_buf(false, true)
+
+	local opts = {
+		relative = "editor",
+		row = win_row,
+		col = win_col,
+		width = win_width,
+		height = win_height,
+		style = "minimal",
+		border = "single",
+	}
+
+	local win = vim.api.nvim_open_win(buf, true, opts)
+
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Running Git command..." })
+
+	for _, use in ipairs(opt.use) do
+		local cmd = installMethods({ mode = opt.mode }, { use = use }) -- 替换成你需要执行的 Git 指令
+		vim.fn.jobstart(cmd, {
+			on_stdout = function(_, data, _)
+				vim.api.nvim_buf_set_lines(buf, -1, -1, false, vim.split(data, "\n"))
+			end,
+			on_stderr = function(_, data, _)
+				vim.api.nvim_buf_set_lines(buf, -1, -1, false, vim.split(data, "\n"))
+			end,
+			on_exit = function(_, exit_code)
+				if exit_code == 0 then
+					vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "Done!" })
+				else
+					vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "Error!" })
+				end
+				vim.api.nvim_win_close(win, true)
+			end,
+		})
+	end
+end
+
 local function SuperSyncdDownload(opt)
+	if opt.progress_bar then
+		progressInstall(opt)
+	end
 	for _, value in ipairs(opt.use) do
-		local mode = Mode(opt.mode)
-		local exists = vim.fn.isdirectory(install_path .. vim.split(value, "/")[2]) == 1
-		if exists then
-			vim.api.nvim_command("!cd " .. install_path .. "/" .. vim.split(value, "/")[2] .. " && git pull")
-		else
-			vim.api.nvim_command(
-				"!git clone " .. mode .. value .. " " .. install_path .. "/" .. vim.split(value, "/")[2]
-			)
-		end
 	end
 end
 
@@ -33,14 +79,6 @@ M.setup = function(config)
 		display = {
 			progress_bar = {
 				enable = false,
-				title = "",
-				title_pos = "center",
-				size = { w = 60, h = 7 },
-				position = "cc",
-				style = {
-					f_color = "",
-					b_color = "",
-				},
 			},
 		},
 		use = {},
