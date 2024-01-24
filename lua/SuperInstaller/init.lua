f48690a9739fb9502d8e10d9281912a517990b07
wukuohao <wukuohao2022@icloud.com>
2024-01-24 13:31:53 +0800

    test

--- lua/SuperInstaller/init.lua
+++ lua/SuperInstaller/init.lua
@@ -16,7 +16,7 @@ end
 local function SuperSyncdDownload(opt)
 	for _, value in ipairs(opt.plugin) do
 		local mode = Mode(opt.mode)
-		if vim.fn.isdirectory(install_path .. vim.split(value, "/")[2]) > 0 then
+		if vim.fn.isdirectory(install_path .. vim.split(value, "/")[2]) == 0 then
 			vim.api.nvim_command(
 				"!git clone " .. mode .. value .. " " .. install_path .. "/" .. vim.split(value, "/")[2]
 			)
