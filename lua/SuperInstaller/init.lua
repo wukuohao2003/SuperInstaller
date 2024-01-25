c88a050143fd9f00a87489234cef6daeb070b9fc
wukuohao <wukuohao2022@icloud.com>
2024-01-25 13:03:34 +0800

    test

--- lua/SuperInstaller/init.lua
+++ lua/SuperInstaller/init.lua
@@ -45,6 +45,7 @@ local function progressInstall(opt)
 	for _, use in ipairs(opt.use) do
 		local job_id = vim.fn.jobstart(installMethods({ mode = opt.mode, use = use }), {
 			on_stdout = function(_, data, _)
+				print(vim.inspect(data))
 				local progress = string.match(vim.inspect(data), "^Receiving objects: (%d+)%%")
 				if progress then
 					print("Download progress: " .. progress .. "%")
