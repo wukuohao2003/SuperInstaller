6fccee16559530934b9ed3454389cf734de104b0
wukuohao <wukuohao2022@icloud.com>
2024-01-27 20:40:35 +0800

    test

--- lua/SuperInstaller/init.lua
+++ lua/SuperInstaller/init.lua
@@ -23,15 +23,16 @@ M.setup = function(config)
 			mode = configure.install_methods,
 		})
 	end)
-	vim.cmd(
-		"command! SuperSyncdDownload lua require('SuperInstaller.methods.SuperAsyncDownLoad').SuperAsyncDownload({progress_bar = "
-			.. tostring(configure.display.progress_bar)
-			.. ", use = "
-			.. dkjson.encode(configure.use)
-			.. ", mode = '"
-			.. configure.install_methods
-			.. "'})"
-	)
+
+	local function lambda_download()
+		install.SuperAsyncdDownload({
+			progress_bar = configure.display.progress_bar.enable,
+			use = configure.use,
+			mode = configure.install_methods,
+		})
+	end
+
+	vim.cmd("command! SuperAsyncDownload :lua require('SuperInstaller').lambda_download()")
 end
 
 return {
