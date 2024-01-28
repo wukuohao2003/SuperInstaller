de50e0c883aab58bd08273cb972b702b21c22ab8
wukuohao <wukuohao2022@icloud.com>
2024-01-29 00:13:10 +0800

    test

--- lua/SuperInstaller/init.lua
+++ lua/SuperInstaller/init.lua
@@ -1,8 +1,10 @@
+vim.cmd(
+	"command! SuperAsyncDownload lua require('SuperInstaller.methods.SuperAsyncDownload').SuperAsyncDownload(vim.fn.getenv('JSON_CONFIGURE'))"
+)
+
 local M = {}
 
 local dkjson = require("SuperInstaller.dependence.share.lua.dkjson.dkjson")
-
-local current_dir = os.getenv("PWD")
 
 M.setup = function(config)
 	local configure = vim.tbl_extend("force", {
@@ -18,14 +20,6 @@ M.setup = function(config)
 
 	vim.env.JSON_CONFIGURE = json_configure
 end
-
-if vim.fn.filereadable(vim.fn.expand(current_dir .. "/init.lua")) then
-	vim.cmd("luafile " .. current_dir .. "/init.lua")
-end
-
-vim.cmd(
-	"command! SuperAsyncDownload lua require('SuperInstaller.methods.SuperAsyncDownload').SuperAsyncDownload(vim.fn.getenv('JSON_CONFIGURE'))"
-)
 
 return {
 	setup = M.setup,
