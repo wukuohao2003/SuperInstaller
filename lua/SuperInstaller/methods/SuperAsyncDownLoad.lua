cbe9fa9ecc387a7452a3e8bf0dd7fe93faced7c2
wukuohao <wukuohao2022@icloud.com>
2024-01-28 23:41:53 +0800

    test

--- lua/SuperInstaller/methods/SuperAsyncDownLoad.lua
+++ lua/SuperInstaller/methods/SuperAsyncDownLoad.lua
@@ -71,9 +71,7 @@ end
 
 M.SuperAsyncDownload = function(opt)
 	local option = dkjson.decode(opt)
-	for _, value in ipairs(option.repositories) do
-		progressInstall(option.git, value)
-	end
+	print(option)
 end
 
 return {
