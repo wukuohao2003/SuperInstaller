7145c6f227f882656e6129645625abfa1fd1f8b2
wukuohao <wukuohao2022@icloud.com>
2024-01-28 23:55:47 +0800

    test

--- lua/SuperInstaller/methods/SuperAsyncDownLoad.lua
+++ lua/SuperInstaller/methods/SuperAsyncDownLoad.lua
@@ -50,6 +50,7 @@ local function progressInstall(mode, use)
 	local result = nil
 	local async_job = vim.fn.jobstart(command, {
 		on_stderr = function(job_id, data, event)
+			print(data)
 			result = string.match(data[1], "^Resolving deltas:  (%d+)%%")
 			if result then
 				vim.api.nvim_buf_set_lines(
